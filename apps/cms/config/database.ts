a24634e0d3443f363bf1b824fc712e2a8ee143fc
Matt Moeller <ultamatt@gmail.com>
2026-02-26 16:10:17 -0800

    refactor: simplify database filename path resolution

--- apps/cms/config/database.ts
+++ apps/cms/config/database.ts
@@ -9,11 +9,7 @@ export default ({ env }: { env: Env }) => {
     connection: {
       client: "sqlite",
       connection: {
-        filename: path.join(
-          __dirname,
-          "..",
-          env("DATABASE_FILENAME", ".tmp/data.db")
-        ),
+        filename: path.join(process.cwd(), env("DATABASE_FILENAME", ".tmp/data.db")),
       },
       useNullAsDefault: true,
       acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
